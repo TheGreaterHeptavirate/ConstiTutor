b9abbc2d3e7f9f8d1122a7f84fbad9ef901a88ce
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-10-23 18:10:33 +0200

    project: fix typo in copyright note

--- cmd/constitutor/generate_windows.go
+++ cmd/constitutor/generate_windows.go
@@ -2,9 +2,9 @@
 // +build windows
 
 /*
- * Copyright (c) 2022. The Greater Heptavirate (https://github.com/TheGreaterHeptavirate). All Rights Reservet
+ * Copyright (c) 2022. The Greater Heptavirate (https://github.com/TheGreaterHeptavirate). All Rights Reserved
  *
- * All copies of this software (if not stated otherway) are dedicated
+ * All copies of this software (if not stated otherwise) are dedicated
  * ONLY to personal, non-commercial use.
  */
 
